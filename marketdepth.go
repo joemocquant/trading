01d3aa36021801515774958d8d246bb580023d9a
Joe <jocelyn.mocquant@gmail.com>
2017-05-08 16:04:08 -0700

    Metrics: market depths

---
+++ marketdepth.go
@@ -0,0 +1,345 @@
+package metrics
+
+import (
+	"encoding/json"
+	"fmt"
+	"strconv"
+	"time"
+	"trading/database"
+
+	"github.com/Sirupsen/logrus"
+	ifxClient "github.com/influxdata/influxdb/client/v2"
+)
+
+type orderBooks map[string]*orderBook
+
+type orderBook struct {
+	asks []*order
+	bids []*order
+}
+
+type order struct {
+	rate          float64
+	cumulativeSum float64
+}
+
+type marketDepths map[string]*marketDepth
+
+type marketDepth struct {
+	bidsDepth map[float64]float64
+	asksDepth map[float64]float64
+}
+
+type getLastOrderBooks func(map[string]time.Time) orderBooks
+
+func computeMarketDepth() {
+
+	cmd := func(period time.Duration, globs getLastOrderBooks, exchange string) {
+
+		previousStarts := make(map[string]time.Time)
+
+		for {
+			obs := globs(previousStarts)
+			mds := getMarketDepths(obs)
+			prepareMarketDepthsPoints(mds, previousStarts, exchange)
+
+			<-time.After(period)
+		}
+	}
+
+	p := time.Duration(conf.Poloniex.MarketDepthPeriodCheckSec) * time.Second
+	go cmd(p, getLastOrderBooksPoloniex, "poloniex")
+
+	p = time.Duration(conf.Bittrex.MarketDepthPeriodCheckSec) * time.Second
+	go cmd(p, getLastOrderBooksBittrex, "bittrex")
+}
+
+func getLastOrderBooksPoloniex(
+	previousStarts map[string]time.Time) orderBooks {
+
+	query := fmt.Sprintf("SELECT ask_depth FROM %s ORDER BY time DESC LIMIT 1",
+		conf.Poloniex.Schema["book_orders_last_check_measurement"])
+
+	res, err := database.QueryDB(
+		dbClient, query, conf.Poloniex.Schema["database"])
+
+	for err != nil {
+		logger.WithFields(logrus.Fields{
+			"error": err,
+			"query": query,
+		}).Error("getLastOrderBooksPoloniex: database.QueryDB")
+
+		time.Sleep(5 * time.Second)
+		res, err = database.QueryDB(
+			dbClient, query, conf.Poloniex.Schema["database"])
+	}
+
+	if len(res[0].Series) == 0 || len(res[0].Series[0].Values) != 1 {
+		logger.Info("getLastOrderBooksPoloniex: no orderBooks found")
+		return nil
+	}
+
+	start, err := time.Parse(
+		time.RFC3339, res[0].Series[0].Values[0][0].(string))
+
+	if err != nil {
+		logger.WithField("error", err).Error(
+			"getLastOrderBooksPoloniex: time.Parse")
+		return nil
+	}
+
+	prevousStart, ok := previousStarts["allMarkets"]
+
+	if ok && prevousStart.Unix() == start.Unix() {
+		return nil
+	} else {
+		previousStarts["allMarkets"] = start
+	}
+
+	end := start.Add(1 * time.Second)
+
+	query = fmt.Sprintf(
+		`SELECT rate, cumulative_sum, order_type FROM %s
+    WHERE time >= %d AND time < %d GROUP BY market`,
+		conf.Poloniex.Schema["book_orders_measurement"],
+		start.UnixNano(), end.UnixNano())
+
+	res, err = database.QueryDB(
+		dbClient, query, conf.Poloniex.Schema["database"])
+
+	for err != nil {
+		logger.WithField("error", err).Error(
+			"getLastOrderBooksPoloniex: database.QueryDB")
+
+		time.Sleep(5 * time.Second)
+		res, err = database.QueryDB(
+			dbClient, query, conf.Poloniex.Schema["database"])
+	}
+
+	return formatOrderBooks(res)
+}
+
+func getLastOrderBooksBittrex(previousStarts map[string]time.Time) orderBooks {
+
+	query := fmt.Sprintf(`SELECT ask_depth FROM %s
+    GROUP BY market ORDER BY time DESC LIMIT 1`,
+		conf.Bittrex.Schema["book_orders_last_check_measurement"])
+
+	res, err := database.QueryDB(
+		dbClient, query, conf.Bittrex.Schema["database"])
+
+	for err != nil {
+		logger.WithFields(logrus.Fields{
+			"error": err,
+			"query": query,
+		}).Error("getLastOrderBooksBittrex: database.QueryDB")
+
+		time.Sleep(5 * time.Second)
+		res, err = database.QueryDB(
+			dbClient, query, conf.Bittrex.Schema["database"])
+	}
+
+	if len(res[0].Series) == 0 {
+		logger.Info("getLastOrderBooksBittrex: no orderBooks found")
+		return nil
+	}
+
+	query = ""
+	for _, serie := range res[0].Series {
+
+		market := serie.Tags["market"]
+
+		start, err := time.Parse(
+			time.RFC3339, serie.Values[0][0].(string))
+
+		if err != nil {
+			logger.WithField("error", err).Error(
+				"getLastOrderBooksBittrex: time.Parse")
+			continue
+		}
+
+		prevousStart, ok := previousStarts[market]
+		if ok && prevousStart.Unix() == start.Unix() {
+			continue
+		} else {
+			previousStarts[market] = start
+			start := previousStarts[market]
+			end := start.Add(1 * time.Second)
+
+			query += fmt.Sprintf(
+				`SELECT rate, cumulative_sum, order_type FROM %s
+      WHERE market = '%s' AND time >= %d AND time < %d GROUP BY market;`,
+				conf.Bittrex.Schema["book_orders_measurement"],
+				market, start.UnixNano(), end.UnixNano())
+		}
+	}
+
+	if query == "" {
+		return nil
+	}
+
+	res, err = database.QueryDB(
+		dbClient, query, conf.Bittrex.Schema["database"])
+
+	for err != nil {
+		logger.WithField("error", err).Error(
+			"getLastOrderBooksBittrex: database.QueryDB")
+
+		time.Sleep(5 * time.Second)
+		res, err = database.QueryDB(
+			dbClient, query, conf.Bittrex.Schema["database"])
+	}
+
+	obs := formatOrderBooks(res)
+	return obs
+}
+
+func formatOrderBooks(res []ifxClient.Result) orderBooks {
+
+	obs := make(orderBooks)
+
+	for _, result := range res {
+		for _, serie := range result.Series {
+
+			market := serie.Tags["market"]
+			ob := orderBook{}
+
+			for _, o := range serie.Values {
+
+				rate, err := o[1].(json.Number).Float64()
+
+				if err != nil {
+					logger.Errorf("formatOrderBooks: Wrong rate type: %v (%s)",
+						rate, market)
+					continue
+				}
+
+				cumulativeSum, err := o[2].(json.Number).Float64()
+
+				if err != nil {
+					logger.Errorf("formatOrderBooks: Wrong cumulativeSum type: %v (%s)",
+						cumulativeSum, market)
+					continue
+				}
+
+				orderType := o[3].(string)
+
+				if orderType == "ask" {
+					ob.asks = append(ob.asks, &order{rate, cumulativeSum})
+				} else {
+					ob.bids = append(ob.bids, &order{rate, cumulativeSum})
+				}
+			}
+
+			if len(ob.bids) != 0 && len(ob.asks) != 0 {
+				obs[market] = &ob
+			}
+		}
+	}
+
+	return obs
+}
+
+func getMarketDepths(obs orderBooks) marketDepths {
+
+	intervals := conf.MarketDepthIntervals
+
+	mds := make(marketDepths, len(obs))
+
+	for market, _ := range obs {
+
+		md := marketDepth{make(map[float64]float64, len(intervals)),
+			make(map[float64]float64, len(intervals)),
+		}
+		mds[market] = &md
+	}
+
+	for market, ob := range obs {
+
+		baseRate := (ob.bids[0].rate + ob.asks[0].rate) / 2
+		cs := 0.0
+		lastI := 0
+
+		for _, interval := range intervals {
+
+			bound := baseRate - (baseRate / 100 * interval)
+
+			for i, bid := range ob.bids[lastI:] {
+
+				if bid.rate < bound {
+					mds[market].bidsDepth[interval] = cs
+					lastI = i
+					break
+				}
+				cs = bid.cumulativeSum
+			}
+		}
+	}
+
+	for market, ob := range obs {
+
+		baseRate := (ob.bids[0].rate + ob.asks[0].rate) / 2
+		cs := 0.0
+		lastI := 0
+
+		for _, interval := range intervals {
+
+			bound := baseRate + (baseRate / 100 * interval)
+
+			for i, ask := range ob.asks[lastI:] {
+
+				if ask.rate > bound {
+					mds[market].asksDepth[interval] = cs
+					lastI = i
+					break
+				}
+				cs = ask.cumulativeSum
+			}
+		}
+	}
+
+	return mds
+}
+
+func prepareMarketDepthsPoints(mds marketDepths,
+	previousStarts map[string]time.Time, exchange string) {
+
+	measurement := conf.Schema["market_depths_measurement"]
+
+	for market, md := range mds {
+
+		baseTimestamp, ok := previousStarts["allMarkets"]
+		if !ok {
+			baseTimestamp = previousStarts[market]
+		}
+
+		points := make([]*ifxClient.Point, 0, len(mds)*2)
+
+		tags := map[string]string{
+			"market":   market,
+			"exchange": exchange,
+		}
+
+		for _, interval := range conf.MarketDepthIntervals {
+
+			tags["interval"] = strconv.FormatFloat(interval, 'f', 2, 64)
+
+			fields := map[string]interface{}{
+				"bid_depth": md.bidsDepth[interval],
+				"ask_depth": md.asksDepth[interval],
+			}
+
+			pt, err := ifxClient.NewPoint(measurement, tags, fields, baseTimestamp)
+			if err != nil {
+				logger.WithField("error", err).Error(
+					"prepareMarketDepthsPoints: ifxClient.NewPoint")
+			}
+			points = append(points, pt)
+		}
+
+		batchsToWrite <- &database.BatchPoints{
+			exchange + "MarketDepth",
+			points,
+		}
+	}
+}
---
+++ metrics.go
@@ -0,0 +1,95 @@
+package metrics
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"time"
+	"trading/database"
+
+	"github.com/Sirupsen/logrus"
+	ifxClient "github.com/influxdata/influxdb/client/v2"
+	prefixed "github.com/x-cray/logrus-prefixed-formatter"
+)
+
+var (
+	conf          *configuration
+	logger        *logrus.Entry
+	dbClient      ifxClient.Client
+	batchsToWrite chan *database.BatchPoints
+)
+
+type configuration struct {
+	metricsConf `json:"metrics"`
+}
+
+type metricsConf struct {
+	LogLevel             string            `json:"log_level"`
+	MarketDepthIntervals []float64         `json:"market_depth_intervals"`
+	Schema               map[string]string `json:"schema"`
+	FlushBatchsPeriodMs  int               `json:"flush_batchs_period_ms"`
+	FlushCapacity        int               `json:"flush_capacity"`
+	Poloniex             exchangeConf      `json:"poloniex"`
+	Bittrex              exchangeConf      `json:"bittrex"`
+}
+
+type exchangeConf struct {
+	Schema                    map[string]string `json:"schema"`
+	MarketDepthPeriodCheckSec int               `json:"market_depth_period_check_sec"`
+}
+
+func init() {
+
+	customFormatter := new(prefixed.TextFormatter)
+	customFormatter.FullTimestamp = true
+	customFormatter.ForceColors = true
+	customFormatter.ForceFormatting = true
+	logrus.SetFormatter(customFormatter)
+
+	logger = logrus.WithField("prefix", "[metrics]")
+
+	content, err := ioutil.ReadFile("conf.json")
+
+	if err != nil {
+		logger.WithField("error", err).Fatal("loading configuration")
+	}
+
+	if err := json.Unmarshal(content, &conf); err != nil {
+		logger.WithField("error", err).Fatal("loading configuration")
+	}
+
+	switch conf.LogLevel {
+	case "debug":
+		logrus.SetLevel(logrus.DebugLevel)
+	case "info":
+		logrus.SetLevel(logrus.InfoLevel)
+	case "warn":
+		logrus.SetLevel(logrus.WarnLevel)
+	case "error":
+		logrus.SetLevel(logrus.ErrorLevel)
+	case "fatal":
+		logrus.SetLevel(logrus.FatalLevel)
+	case "panic":
+		logrus.SetLevel(logrus.PanicLevel)
+	default:
+		logrus.SetLevel(logrus.WarnLevel)
+	}
+
+	if dbClient, err = database.NewdbClient(); err != nil {
+		logger.WithField("error", err).Fatal("database.NewdbClient")
+	}
+
+	batchsToWrite = make(chan *database.BatchPoints, conf.FlushCapacity)
+}
+
+func ComputeMetrics() {
+
+	// flushing batchs periodically
+	period := time.Duration(conf.FlushBatchsPeriodMs) * time.Millisecond
+	go database.FlushEvery(period, &database.FlushInfo{
+		batchsToWrite,
+		conf.Schema["database"],
+		dbClient,
+	})
+
+	go computeMarketDepth()
+}
